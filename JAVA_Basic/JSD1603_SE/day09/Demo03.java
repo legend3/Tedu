96e4780a03c72f4c6710180fb800e223bc15e275
legend3 <zhenguo50@126.com>
2021-09-15 03:51:11 +0800

    字符流

--- JAVA_Basic/JSD1603_SE/day09/Demo03.java
+++ JAVA_Basic/JSD1603_SE/day09/Demo03.java
@@ -5,19 +5,15 @@ import java.io.FileInputStream;
 import java.io.ObjectInputStream;
 
 public class Demo03 {
-
-	public static void main(String[] args)
-		throws Exception{
-		FileInputStream fis = 
-			new FileInputStream("obj.dat");
-		ObjectInputStream in =
-			new ObjectInputStream(
-			new BufferedInputStream(fis));
+	public static void main(String[] args) throws Exception{
+		FileInputStream fis = new FileInputStream("obj.dat");
+		ObjectInputStream in = new ObjectInputStream(
+			new BufferedInputStream(fis)
+		);
 		/*
 		 * 调用对象的反序列化方法读取对象
-		 * 持久化
 		 */
-		Foo f = (Foo)in.readObject();
+		Foo f = (Foo)in.readObject();//序列化对象转对象，反序列化
 		in.close();
 		System.out.println(f); 
 	}
--- JAVA_Basic/JSD1603_SE/day09/Demo04.java
+++ JAVA_Basic/JSD1603_SE/day09/Demo04.java
@@ -3,15 +3,16 @@ package day09;
 import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 
+/**
+ * 字符流：是高级流，必须依赖低级的byte流，字符流封装了字符的编码和解码功能，能够将字符数据编码序列化为byte写到低级流中，或将byte字符数据解码为字符数据。
+ */
 public class Demo04 {
 
-	public static void main(String[] args) 
-		throws Exception{
+	public static void main(String[] args) throws Exception{
 		String s = "你吃了吗？\n";
-		FileOutputStream fos = 
-			new FileOutputStream("d.txt");
-		OutputStreamWriter writer = 
-			new OutputStreamWriter(fos,"UTF-8");
+//		FileOutputStream fos = new FileOutputStream("d.txt");
+		OutputStreamWriter writer = new OutputStreamWriter(
+				new FileOutputStream("d.txt"),"UTF-8");
 		//字符流将字符数据编码为byte写到低级流
 		writer.write(s);
 		writer.close();
