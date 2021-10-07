c4935fae7a9053ffb9e820f610be635f26680f54
legend3 <zhenguo50@126.com>
2021-10-07 18:49:12 +0800

    枚举类中还可以有抽象方法

--- JAVA_Basic/JSD1603_SE/enumDemo/case01/CodeEnum.java
+++ JAVA_Basic/JSD1603_SE/enumDemo/case01/CodeEnum.java
@@ -6,9 +6,24 @@ package enumDemo.case01;
  */
 public enum CodeEnum implements SystemCode{
     //枚举项
-    SUCCESSFUL(0, "操作成功！"),
-    ERROR(-1, "系统错误"),
-    FAILURE(1, "操作成功！");
+    SUCCESSFUL(0, "操作成功！"){
+        @Override
+        public void fun() {
+            System.out.println("SUCCESSFUL枚举项重写fun抽象方法");
+        }
+    },
+    ERROR(-1, "系统错误"){
+        @Override
+        public void fun() {
+            System.out.println("ERROR枚举项重写fun抽象方法");
+        }
+    },
+    FAILURE(1, "操作成功！"){
+        @Override
+        public void fun() {
+            System.out.println("FAILURE枚举项重写fun抽象方法");
+        }
+    };
 
     private CodeEnum() {
     }
@@ -31,7 +46,11 @@ public enum CodeEnum implements SystemCode{
         return null;
     }
 
-    public static void main(String[] args) {
-
-    }
+    /*
+    枚举类中还可以有抽象方法
+    还可以在枚举类中给出抽象方法，然后在创建每个枚举项时使用“特殊”的语法来重复抽象方法。所谓“特殊”语法就是匿名内部类！也就是说每个枚举项都是一个匿名类的子类对象！
+    通常fun()方法应该定义为抽象的方法，因为每个枚举常量都会去重写它。
+    你无法把CodeEnum声明为抽象类，但需要声明fun()方法为抽象方法。
+     */
+    public abstract void fun();//只需要把fun()方法修改为抽象方法，但不可以把CodeEnum类声明为抽象类。
 }
