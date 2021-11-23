e4097175532b57ffa4b3b1879557085635338f7b
legend3 <zhenguo50@126.com>
2021-11-23 20:25:18 +0800

    内部类的总结

--- JAVA_Basic/01JAVA_BASIC_JSD1603/src/Oop/day06/InnerClassDemo.java
+++ JAVA_Basic/01JAVA_BASIC_JSD1603/src/Oop/day06/InnerClassDemo.java
@@ -4,39 +4,68 @@ package Oop.day06;
 public class InnerClassDemo {
 	public static void main(String[] args) {
 		Mama m = new Mama();
-		m.createBaby().showMamaName();
+//		m.createBaby().showMama();
 		//Baby b = new Baby(); //编译错误，内部类对外不具备可见性
+
+		//外部类创建内部类
+		Mama.Baby baby01 = m.new Baby(2);//注意这种创建形式
+		Mama.Baby baby02 = m.new Baby(5);//注意这种创建形式
+		baby01.setName("baby01");
+		baby02.setName("baby02");
+		baby01.start();
+		baby02.start();
 	}
 }
 
 class Mama{ //外部类
 	private String name;
+	private int age = 30;
+	private static String id;
 
 	Baby createBaby(){
-		return new Baby(10); //内部类对象通常在外部类中创建
+		return new Baby(2); //内部类对象通常在外部类中创建
+	}
+
+	public void cook(){
+		System.out.println("做饭");
+	}
+	public static void wash(){
+		System.out.println("洗衣服");
 	}
 
 	public static void main(String[] args) {
 		Mama m = new Mama();
 		System.out.println(m.createBaby().age);//外部类调用内部类属性
-		m.createBaby().showMamaName();//外部类调用内部类方法
+		m.createBaby().showMama();//外部类调用内部类方法
+
 	}
 
 	//自我总结: 如何类中要频繁重复使用某一些属性、或方法且这些属性与方法与类属性间有调用关联，
 	// 就把它们封装在一个成员内部类中，避免新建类、再调用！
-	class Baby{ //内部类
+	class Baby extends Thread{ //内部类
 		private int age;//成员内部类自己的私有属性
 
 		public Baby(int age) {
 			this.age = age;
 		}
-		public String showMamaName(){
-			name = "Baby`s name!";//1.可直接访问外部类成员变量
+
+		@Override
+		public void run() {
+			System.out.println(Thread.currentThread().getName() + "通过内部类实现多继承...");
+		}
+
+		public String showMama(){
+//			Mama.this.cook();//隐式调用外部类的非静态方法
+			cook();//直接调用外部类的非静态方法
+			Mama.wash();//调外部类静态方法
+			name = "Baby`s name!";//1.可直接访问、修改外部类成员变量
 			System.out.println("直接访问外部类成员变量：" + name);
 			System.out.println("隐式的引用指向创建的它的外部类对象访问成员变量：" + Mama.this.name);//2.内部类有个隐式的引用指向了创建它的外部类对象
 												// 外部类名.this.
 			//System.out.println(this.name); //编译错误，Baby类中不包含name
+			System.out.println("外部类的age属性值：" + Mama.this.age);
 			System.out.println("成员内部类的age属性值: " + this.age);
+			System.out.println("成员内部类的age属性值: " + age);
 			return name;
 		}
 	}
---
+++ JAVA_Basic/01JAVA_BASIC_JSD1603/src/Oop/day06/staticInnerClassDemo.java
@@ -0,0 +1,43 @@
+package Oop.day06;
+
+public class staticInnerClassDemo {
+    public static void main(String[] args) {
+        Leader leader = new Leader();
+        Leader.Staff staff = new Leader.Staff("油漆工");
+        staff.showLeader();
+        staff.run();
+    }
+}
+class Leader {
+    private static String lname;
+    private int age;
+
+    public static void do01() {
+        System.out.println("领导的静态工作");
+    }
+    public void do02() {
+        System.out.println("领导的普通工作");
+    }
+
+    static class Staff extends Thread{
+        private String name;
+        private int age;
+
+        public Staff(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public void run() {
+            System.out.println("通过内部类是外部类实现了多继承...");
+        }
+
+        public void showLeader() {
+            lname = "大boss";
+            System.out.println("领导的名字：" + lname);
+            System.out.println("员工自己的名字：" + name);
+            new Leader().do02();//调用外部类的非方法，要用外部类的实例调.
+            do01();//调用外部类的静态方法
+        }
+    }
+}
